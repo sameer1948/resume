ee999cd16201a4d8a49e29168bb3d31738b2c0e6
Sameer <sameer.sameer185@gmail.com>
2024-12-12 23:27:06 +0530

    Update Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,16 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user = {
+    username: ''
+  };
 
-  title = 'Restaurant - web';
-
-  constructor(private titleService: Title, private router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      const pageTitle = this.getPageTitle(this.router.url);
-      this.setTitle(pageTitle);
-    });
+  submitForm() {
+    console.log(this.user);
   }
-
-  setTitle(newTitle: string) {
-    this.titleService.setTitle(newTitle);
-  }
-
-  getPageTitle(url: string): string {
-    switch (url) {
-
-      case '/home':
-        return 'Home';
-
-      case '/members':
-        return 'Members';
-
-      case '/profile':
-        return 'Profile';
-
-      case '/login':
-        return 'Login';
-
-      case '/error':
-        return 'Error';
-
-      case '/coupon':
-        return 'Coupon';
-
-      case '/settings':
-        return 'Settings';
-
-      case '/tax':
-        return 'Tax & Fee';
-
-      case '/unauthorized':
-        return 'Unauthorized';
-
-      case '/about':
-        return 'About Us';
-
-      default:
-        return this.title;
-    }
-  }
-
 }
